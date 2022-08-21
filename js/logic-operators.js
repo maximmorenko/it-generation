73ba8c4880ce1d5610c6f0e8158e4f9f28cd4a6d
Maksym Morenko <morenko.maxim@gmail.com>
2022-08-21 20:58:32 +0300

    Логічні оператори (&& || !)

--- js/logic-operators.js
+++ js/logic-operators.js
@@ -6,17 +6,31 @@
 //* Якщо операнд був преведенний до false, то повертається значення цього операнду.
 //* Якщо всі операнди були приведені до true, то повертається значення останнього операнду.
 
-const result = 0 && 20 && 'Hello';
+const result = 0 && 20 && 'Hello'; //0
+const result2 = 2 && 20 && 'Hello'; //Hello
 
-console.log(result);
+// console.log(result);
+// console.log(result2);
 
 //* || - запинається на правді
 //* Якщо операнд був преведенний до true, то повертається значення цього операнду.
 //* Якщо всі операнди були приведені до false, то повертається останній.
 
-const result2 = 0 || '' || null;
+const result3 = 0 || '' || null; //null
 
-console.log(result2);
+// console.log(result3);
 
 //* ! - інвертує правду до брехні, а брехню до правди
 // console.log(!'Hello');
+
+console.log(Boolean(0)); //false
+
+console.log(Boolean(''));//false
+
+console.log(Boolean(null));//false
+
+console.log(Boolean(1)); //true
+
+console.log(Boolean('Hello'));//true
+
+console.log(Boolean());//false
